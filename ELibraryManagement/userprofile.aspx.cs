c19268459eea68a030763b3f79af67b4ebb256f3
Jay-Dalsaniya <dalsaniyajay111@gmail.com>
2024-07-11 19:04:07 +0530

    add project one folder

---
+++ ELibraryManagement/userprofile.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ELibraryManagement
+{
+    public partial class userprofile : System.Web.UI.Page
+    {
+        string strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // user login
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+
+
+            Response.Write("<script>alert('button click');</script>");
+        }
+
+    }
+
+}
